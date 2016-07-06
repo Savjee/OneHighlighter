42b3a02a938efb25654a1e9986984a91f2c7d093
Xavier Decuyper <hi@savjee.be>
2016-07-06 15:02:28 +0200

    Restructuring: putting the source code in src/

--- src/main.js
+++ src/main.js
@@ -1,32 +1,16 @@
-var menubar = require('menubar');
+const menubar = require('menubar');
 const {clipboard} = require('electron');
-const ipcMain = require('electron').ipcMain;
 
-ipcMain.on('ping', (event, arg) => {
-    // open new browser window
-    console.log('pin!!');
-});
-
-// ipcMain.send('poo');
-
-var mb = menubar({
+let mb = menubar({
     dir: __dirname,
     transparent: true
 });
 
-
 mb.on('ready', function ready () {
     console.log('app is ready');
-
-    console.log(mb);
-    // mb.app.send('ping', 'whooo');
-    // your app code here
 });
 
 mb.on('after-create-window', function () {
-    let html = mb.window.webContents;
-
-
     // html.openDevTools({mode: 'detach'});
 });
 
